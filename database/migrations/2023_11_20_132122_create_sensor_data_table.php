a34fb9d60c0ac51be36d7aac399cffdf3a9a46f1
Irfan Hanif <14.irfanh@gmail.com>
2023-11-20 20:37:36 +0700

    created migration files for sensor

---
+++ database/migrations/2023_11_20_132122_create_sensor_data_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sensor_data', function (Blueprint $table) {
+            $table->id();
+            $table->integer('availability');
+            $table->integer('utilization');
+            $table->unsignedBigInteger('sensor_id');
+            $table->foreign('sensor_id')->references('id')->on('sensors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sensor_data');
+    }
+};
